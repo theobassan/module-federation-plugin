61d1287b72af34bba711a98cb8fcf3b5db446100
theobassan <theobassan@gmail.com>
2021-09-30 14:25:22 -0300

    keep license comment on index file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-// This project is libre, and licenced under the terms of the
-// DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
-// as published by dtf on July 2019. See the COPYING file or
-// https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
+/*!
+   This project is libre, and licenced under the terms of the
+   DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
+   as published by dtf on July 2019. See the COPYING file or
+   https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
+*/
 
 import { ModuleFederationPlugin } from './module-federation-plugin';
 import { ModuleFederationExposeTypesPlugin } from './module-federation-expose-plugin';
