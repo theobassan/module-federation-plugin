436a882c1af27a6e45d3fd77c2a17a8c75a05636
theobassan <theobassan@gmail.com>
2021-09-30 14:03:07 -0300

    download type files at remote module folder, not @types

--- src/download.ts
+++ src/download.ts
@@ -6,7 +6,7 @@ const download = async (remotes: Record<string, string>): Promise<void> => {
     for (const [remoteModuleName, remoteModulePath] of Object.entries(remotes)) {
         console.log(`[ModuleFederationRemoteTypesPlugin] downloading module "${remoteModuleName}" type files`);
 
-        const outputPath = `node_modules/@types/${remoteModuleName}`;
+        const outputPath = `node_modules/${remoteModuleName}`;
 
         const splited = remoteModulePath.split('@')[1].split('/');
         splited.pop();
