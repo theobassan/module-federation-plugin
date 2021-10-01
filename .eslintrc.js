d60d55df77c816eb65ac93fabddea7c04c52498e
theobassan <theobassan@gmail.com>
2021-10-01 18:09:17 -0300

    add no-explicit-any rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     rules: {
         'no-null/no-null': 2,
         '@typescript-eslint/no-unused-vars': 2,
+        '@typescript-eslint/no-explicit-any': 2,
         '@theobassan/max-params': ['error', { max: 1, allowArraysCallbacks: true }],
         'max-depth': ['error', { max: 3 }],
         'max-lines': ['error', { max: 60 }],
